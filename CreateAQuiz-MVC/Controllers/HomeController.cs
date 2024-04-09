eb918ca5955a161ddc1cde39fa741e10b5239973
burakgunce <brk.gnc9@hotmail.com>
2024-04-09 22:37:19 +0300

    controllers have been written

--- CreateAQuiz-MVC/Controllers/HomeController.cs
+++ CreateAQuiz-MVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using CreateAQuiz_MVC.Models;
+﻿using CreateAQuiz_MVC.Context;
+using CreateAQuiz_MVC.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,6 +8,18 @@ namespace CreateAQuiz_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly CreateAQuizDb _createAQuizDb;
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(CreateAQuizDb createAQuizDb, UserManager<AppUser> userManager)
+        {
+            _createAQuizDb = createAQuizDb;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
     }
 }
