22e04f37cdcfa82004b98403814b5b2a58819327
burakgunce <brk.gnc9@hotmail.com>
2024-04-11 02:17:44 +0300

    rehber api structure has been prepared

---
+++ RehberWebApi/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RehberWebApi.Models.DTOs;
+using RehberWebApi.Models.Entities;
+
+namespace RehberWebApi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RehberDto, Rehber>();
+        }
+    }
+}
